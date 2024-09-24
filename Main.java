72cf6c89dfdafb56ed1dbcbe32c53257a43e6a66
Tony Dias Jr <141673033+tonydiasjr@users.noreply.github.com>
2024-09-24 11:19:01 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,60 +1,46 @@
-//TD 09/19/24
-//AnimalandHyena.java
+//TD 09/24/24
+//arrayListPractice.java
 
-import java.io.BufferedReader;
-        import java.io.FileReader;
-                import java.io.IOException;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\nWelcome to my Zoo Program!");
 
-        //open the arriving animals file
-        try{
-        BufferedReader bufferedReader = new BufferedReader(new FileReader("C:\\Users\\Tony Jr\\OneDrive\\cit-63\\arrivingAnimals.txt"));
-        String myLine;
+        System.out.println("Hello and welcome!");
+        //create a array list of strings
+        ArrayList<String>myListOfStrings=new ArrayList<>();
+        //add a few elements
+        myListOfStrings.add("first");
+        myListOfStrings.add("second");
+        myListOfStrings.add("third");
+        //output our array
 
-        // read the file one line at time
+        System.out.println("\n myListOfStrings is:" + myListOfStrings);
+        //remove a element
+        myListOfStrings.remove(0);
+        //output our array
+        System.out.println("\n After removing...");
+        System.out.println("\n myListOfStrings is:" + myListOfStrings);
+        //remove a element
+        myListOfStrings.remove("third");
+        //output our array
+        System.out.println("\n After removing...");
+        System.out.println("\n myListOfStrings is:" + myListOfStrings);
 
-            while ((myLine=bufferedReader.readLine())!=null){
-                System.out.println(myLine);
-
-            }
+        //use a for loop to add some things to our list
+        String toAdd ="";
+        for (int i=0;i<10;i++){
+            toAdd =toAdd + "adding..."+ Integer.toString(i);
+            myListOfStrings.add(toAdd);
         }
-        catch (IOException e){
-            throw new RuntimeException(e);
+        System.out.println("\n After adding a bunch of stuff...");
+        System.out.println("\n myListOfStrings is:" + myListOfStrings);
+
+        for (int i=0;i<myListOfStrings.size();i++){
+            System.out.println("An element in my list is: "+myListOfStrings.get(i));
         }
-
-
-
-        //create a Animal object
-        Animal myAnimalObject = new Animal();
-
-
-
-        myAnimalObject.setAge(4);
-
-        //use our new Animal object
-
-        System.out.println("\n my animal age is: "+myAnimalObject.getAge()+" years old");
-
-        //create a Hyena object
-        Hyena myNewHyena = new Hyena();
-        myNewHyena.setAge(7);
-
-        // output the age of our Hyena
-
-        System.out.println("\n the age of our Hyena is : "+ myNewHyena.getAge()+" years old");
-
-        //create a Lion object
-        Lion myLion = new Lion();
-
-        //make the lion 12 years old
-        myLion.setAge(12);
-
-        System.out.println("\n The age of our Lion is : "+ myLion.getAge()+ " years old");
-
-        //output the numbers of animals
-        System.out.println("\n The number of animals created is: "+Animal.numOfAnimals);
 
     }
 }
